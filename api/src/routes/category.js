8499a20700ff9c33d620485d1130643bb6d77d96
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-20 19:29:01 -0300

    (S20): create route put category

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -9,4 +9,21 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+server.put('/category/:id', (req,res)=>{
+    Category.update(
+        {
+            name: req.body.name,
+            description: req.body.description
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(result => {
+        res.status(201).send(result)
+    })
+})
+
 module.exports = server;
