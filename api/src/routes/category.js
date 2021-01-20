34474cbf130f7a9b458fdc434b3590c3c0e238a6
laura <laura.aristegui@hotmail.com>
2021-01-20 16:29:03 -0300

    s18:feature-addcategory

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -9,4 +9,22 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+
+server.post('/', (req, res, next) => {
+   const {name, description} = req.body;
+
+    Category.create({
+	   name:name,
+	   description:description
+   })
+     .then((data) =>{
+		 res.status(200).send('category was added successfully')
+	 })
+	 .catch((err) => {
+		 console.log(err);
+	 });
+});
+
+
+
 module.exports = server;
