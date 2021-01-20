66e9e70dbe949fbdf619776c90a3a2c12b0344f2
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-20 09:12:33 -0300

    (feature/configInitial): cambie el port y el nombre de base de datos por variables de entorno, se deben definir en el .env

--- api/index.js
+++ api/index.js
@@ -19,10 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {
+  PORT_API
+} = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT_API}`); // eslint-disable-line no-console
   });
 });
