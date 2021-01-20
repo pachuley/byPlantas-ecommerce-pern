38ee3e03b21169621a34834f986ba4eea294d058
HomeroHerrero4r6 <homero.herrero.92@gmail.com>
2021-01-20 19:03:29 -0300

    Se trabajo en ruta product

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -11,31 +11,61 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+// server.post('/add', (req, res) =>{
+
+// 	const addProduct = req.body;
+// 	Product.create({
+// 		id:addProduct.id,
+// 		nameProduct: addProduct.nameProduct,
+// 		descriptionProduct: addProduct.descriptionProduct,
+// 		priceProduct: addProduct.priceProduct,
+// 		stockProducts: addProduct.stockProducts,
+// 		urlProducts: addProduct.urlProduct,
+// 		createdAt:addProduct.createdAt,
+// 		updatedAt:addProduct.updatedAt
+// 	})
+// 	.then(response=>res.send(response));
+// })
+server.get('/delete/:id',(req,res)=>{
+	let id = req.params.id;
+	Product.destroy({
+		where:{id:id}
+	}).then(function(result){
+		if(result){
+			res.redirect(200,'/products');
+		}else{
+			res.send('El registro no concuerda con ninguno dentro de la tabla products');
+		}
+	})
+})
+server.get('/:id',(req,res)=>{
+	let id = req.params.id;
+	Product.findAll({
+		where:{id:id}
+	}).then(function(result){
+		if(result){
+			res.send(result);
+		}else{
+			res.send('El registro no concuerda con ninguno dentro de la tabla products');
+		}
+	})
+})
 server.post('/add', (req, res) =>{
 
 	const addProduct = req.body;
-	
-	
-
-	return Product.create({
-
+	Product.create({
+		id:addProduct.id,
 		nameProduct: addProduct.nameProduct,
 		descriptionProduct: addProduct.descriptionProduct,
 		priceProduct: addProduct.priceProduct,
-		stockProduct: addProduct.stockProduct,
-		urlProduct: addProduct.urlProduct,
-
-		
-
+		stockProducts: addProduct.stockProducts,
+		urlProducts: addProduct.urlProduct,
+		createdAt:addProduct.createdAt,
+		updatedAt:addProduct.updatedAt
 	})
-
-	.then(response=>{
-		console.log(response)
-		res.render("hola")
-	})
-	
-
+	.then(response=>res.send(response));
 })
+
 
 module.exports = server;
 
