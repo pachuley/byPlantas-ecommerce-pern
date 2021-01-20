d2f3120c52c6f82ef15d4bd50756608d55d749ba
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-20 15:08:29 -0300

    (fix): fix category routes

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,6 +1,5 @@
 const server = require('express').Router();
 const { Category } = require('../db.js');
-const Category = require('../models/Category.js');
 
 server.get('/', (req, res, next) => {
 	Category.findAll()
