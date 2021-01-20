323e6bfce9193c8b399944485140822c1c8ba991
Lucas <laguero90@gmail.com>
2021-01-20 14:38:56 -0300

    Product Model Created

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,12 +1,38 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('product', {
-    name: {
-      type: DataTypes.STRING,
+  const Product = sequelize.define('product', {
+    nameProduct: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+
+    },
+    descriptionProduct: {
+      type: DataTypes.TEXT,
       allowNull: false,
     },
+    priceProduct: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stockProducts: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    urlProducts: {
+      type: DataTypes.TEXT,
+
+    },
+
   });
 };
+
+
+
+// DATABASE CREATION
+
+// Product.belongsToMany(Category, { through: 'CategoriesXProducts' })
