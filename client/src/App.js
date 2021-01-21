b38d817b97dd73e0e1ddd0c838b4886109faa710
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-21 18:09:09 -0300

    (s14-s15) create route react with catalog and product

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Switch, Link, Route} from 'react-router-dom'
+import Product from './Components/Product/Product'
+import Catalog from './Components/Catalog/Catalog'
 
 function App() {
   return (
-    <div>
+    <>
+      <Switch>
+        <Route path="/products" exact render={() => <Catalog/>}/>
 
-    </div>
+        <Route path='/products/:id' render={({match}) => <Product match={match}/>}/>
+      </Switch>
+
+
+    </>
   );
 }
+
+ 
 
 export default App;
