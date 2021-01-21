228e4c6742ca6a2fcd0d06b5bee4e5fdd9b6a841
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-21 18:16:44 -0300

    (s15): add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {Switch, Link, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Switch, Route} from 'react-router-dom'
 import Product from './Components/Product/Product'
 import Catalog from './Components/Catalog/Catalog'
 
@@ -9,7 +10,6 @@ function App() {
     <>
       <Switch>
         <Route path="/products" exact render={() => <Catalog/>}/>
-
         <Route path='/products/:id' render={({match}) => <Product match={match}/>}/>
       </Switch>
 
