266b07467a5fd9cec422431b077b06da88c03717
pachuley <fjsabate@gmail.com>
2021-01-22 11:05:13 -0300

    (fix) se borro una linea del tope de la searchbar

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from 'axios';
-require('dotenv').config();
 const {REACT_APP_BACKEND_URL} = process.env;
 
 export class SearchBar extends Component {
