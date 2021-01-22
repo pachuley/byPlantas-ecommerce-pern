8294dbfe137590635cede0ae4855ce2cc1cd62d5
unknown <lucas.a.fernandez.morales@gmail.com>
2021-01-22 15:00:44 -0300

    S16(feature)modified FromProduct-addCategoryChecks Function, Added Bootstrap to Package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <div>
 
     </div>
+=======
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Switch, Route} from 'react-router-dom'
+import FormProduct from './Components/FormProduct/FormProduct';
+import FormCategory from './Components/FormCategory/FormCategory';
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route path='/products' exact render={() => <FormProduct/>}/>
+        <Route path='/products/category' render={() => <FormCategory/>}/>
+      </Switch>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
---
+++ client/src/Components/FormProduct/FormProduct.jsx
@@ -0,0 +1,114 @@
+import React,{useEffect, useState}from 'react';
+import axios from 'axios';
+const {REACT_APP_BACKEND_URL} = process.env;
+
+
+const FormProduct = (props) => {
+  const [producto, setProducto] = useState({
+      nameProduct:props ? props.nameProduct : "",
+      descriptionProduct:props ? props.descriptionProduct : "",
+      priceProduct: props ? props.priceProduct :"",
+      stockProduct : props ? props.stockProduct : ""
+  })
+  const [categories, setCategories] = useState([])
+  const [checks, setChecks] = useState([])
+  
+  //este useeffect agarra las categorias y las guarda en el estado categories
+  useEffect(()=>{
+    axios.get(`${REACT_APP_BACKEND_URL}/products/category`)
+    .then(resp=>{setCategories(resp.data)})
+    .catch(err=>{console.log(err)})
+  }, [])
+
+  //este handleinput cambia el valor del estado producto a medida que se escribe en los input del front
+  const handleInputChange = (e) =>{
+    setProducto({...producto,
+      [e.target.name] : e.target.value })
+  }
+  //este handleclick agrega los input checkbox que este con la propiedad checked en true al estado checks 
+  //y los saca cuando la propiedad checked esta en false
+  const handleClick = e => {
+    if(e.target.checked){
+      setChecks([...checks, {name:e.target.name, id:e.target.id}])
+    }else{
+      const newChecks = checks.filter(x=>x.name!==e.target.name)
+      setChecks(newChecks)
+    }
+  }
+
+  //este es el handleSubmit que al usar el boton submit hace un post y agrega el producto a la base de datos,
+  // y si hay inputs checkbox con la propiedad checked en true, hace un post por cada check en el estado checks para agregar las categorias al producto
+  const agregarProducto = (e) =>{
+    e.preventDefault()
+    var prodId;
+    axios.post(`${REACT_APP_BACKEND_URL}/products`, producto)
+    .then(res => {
+      console.log(res)
+      prodId = res.data.id;
+      if(checks.length > 0){
+        checks.forEach(x=>{
+          axios.post(`${REACT_APP_BACKEND_URL}/products/${prodId}/category/${x.id}`)
+          .then(resp=>{console.log(resp)})
+        })
+      }
+    })
+    .catch(err => {console.log(err)})
+  }
+
+  return (
+   
+      <form onSubmit={agregarProducto}>
+    <div>
+      <label>name</label>
+     <input 
+     type="text"
+     name="nameProduct"
+     className="form-control mb-2"
+     placeholder="Ingrese Producto"
+     onChange={handleInputChange}
+     value={producto.nameProduct}
+     />
+    <label>Descripción</label>
+     <input 
+     type="text"
+     name="descriptionProduct"
+     className="form-control mb-2"
+     placeholder="Ingrese Descripción"
+     onChange={handleInputChange}
+     value={producto.descriptionProduct}
+     />
+     <label>Precio</label>
+     <input
+     type="text"
+     name="priceProduct"
+     className="form-control mb-2"
+     placeholder="Ingrese precio"
+     onChange={handleInputChange}
+     value={producto.priceProduct}
+     />
+      <label>Stock</label>
+     <input
+     type="text"
+     name="stockProduct"
+     placeholder=" Ingrese Stock"
+     className="form-control mb-2"
+     onChange={handleInputChange}
+     value={producto.stockProduct}
+     />
+     </div>
+     <label>Categories</label>
+       {categories.map((x,index)=>{
+        return(
+          <div key={index}>
+            <input type='checkbox' id={x.id} name={x.name} onClick={handleClick}/>
+            <label htmlFor={x.id}>{x.name}</label>
+          </div>
+        )
+       })}
+     <button className="btn btn-primary btn-block" type="submit">Agregar</button>
+      </form>
+  )
+}
+
+
+export default FormProduct
