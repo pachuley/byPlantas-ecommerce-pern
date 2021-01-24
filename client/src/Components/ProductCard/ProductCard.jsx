4fc313fb4fb59d94ef68d04c26983352b26126ce
Lucas <laguero90@gmail.com>
2021-01-24 16:45:29 -0300

    bootstrap changes Product card and product detail

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -4,19 +4,32 @@ import { Link } from 'react-router-dom';
 
 const ProductCard = (props) =>{
 
-const { nameProduct, descriptionProduct, priceProduct, urlProduct, stockProduct, id} = props;
+const { nameProduct, descriptionProduct, priceProduct, urlProduct, stockProducts, id} = props;
     console.log(props)
+    let newSource = ""
+    urlProduct ? newSource=urlProduct : newSource="https://cdn.iconscout.com/icon/premium/png-256-thumb/coming-soon-label-842108.png";
     return(
   
-        <div className={`${style.boxes}`}>
-            <Link to={`/products/${id}`}>
-            <img className={`${style.img}`} src={urlProduct}/>
-            <ul>{nameProduct}</ul>
+        <div className="card">
+  <img  src={newSource} className="img-thumbnail" />
+  <div className="card-body">
+    <h5 className="card-title">{nameProduct}</h5>
+    <p className="card-text"> Stock: {stockProducts}</p>
+    <Link to={`/products/${id}`}>
+    <a className="btn btn-primary">Product Details</a>
+    </Link>
+  </div>
+</div>
+        
+        // <div className={`${style.boxes}`}>
+        //     <Link to={`/products/${id}`}>
+        //     <img className={`${style.img}`} src={urlProduct}/>
+        //     <ul>{nameProduct}</ul>
             
-            <ul>ARS {priceProduct}</ul>
-            <ul>{stockProduct}</ul>
-            </Link>
-        </div>
+        //     <ul>ARS {priceProduct}</ul>
+        //     <ul>{stockProduct}</ul>
+        //     </Link>
+        // </div>
         
 
 
--- client/src/Components/ProductDetail/ProductDetail.jsx
+++ client/src/Components/ProductDetail/ProductDetail.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import style from './productDetail.module.css'
 
-const ProductDetail = (props) =>{
 
-const { nameProduct, descriptionProduct, priceProduct, urlProduct, stockProduct, _id} = props;
-console.log(props)
+const ProductDetail = (detail) =>{
+
+// const { nameProduct, descriptionProduct, priceProduct, urlProduct, stockProduct, _id} = props;
+console.log(detail)
     return(
-//  <Link/>
-        <div className={`${style.boxes}`}>
-            <img className={`${style.img}`} src={urlProduct}/>
-            <ul>{nameProduct}</ul>
-            <ul>{descriptionProduct}</ul>
-            <ul>ARS {priceProduct}</ul>
-            <ul>{stockProduct}</ul>
-            <button onClick={()=>alert("Not so fast!")}>Add to Cart</button>
-        </div>
+
+
+<div className="card mb-3">
+<img src="https://cdn.iconscout.com/icon/premium/png-256-thumb/coming-soon-label-842108.png" className="card-img-top" />
+<div className="card-body">
+  {/* <h5 className="card-title"> {nameProduct} </h5>
+  <p className="card-text">{descriptionProduct}</p> */}
+
+  <button type="button" className="btn btn-outline-success">Add</button>
+  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+</div>
+</div>
         
-//  <Link/>
 
     )
 }
 
 
 export default ProductDetail;
+
+ {/* <div className={`${style.boxes}`}>
+            <img className={`${style.img}`} src={urlProduct}/>
+            <ul>{nameProduct}</ul>
+            <ul>{descriptionProduct}</ul>
+            <ul>ARS {priceProduct}</ul>
+            <ul>{stockProduct}</ul>
+            <button onClick={()=>alert("Not so fast!")}>Add to Cart</button>
+        </div> */}
