56c7dfd1a1916388e7dce8e440ec31656718e805
pachuley <fjsabate@gmail.com>
2021-01-24 21:05:14 -0300

    Navbar simplificada para demo

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Navbar.css';
-
+import './navbar.module.css'
 export default function NavBar() {
     return (
         <header id="header" class="fixed-top">
-            <div class="container-fluid">
-                <div class="row justify-content-center">
-                    <div class="d-flex align-items-center">
-                        <h1 ><a href="index.html">byPlantas</a></h1>
-                        <nav >
-                            <ul>
-                                <li class="active">
-                                <NavLink exact to="/" >Inicio</NavLink>
-                                </li>
-                                <li>
-                                <NavLink exact to="/catalogo" >Catálogo</NavLink>
-                                </li>
-                                <li>
-                                <NavLink exact to="/categorias" >Categorias</NavLink>
-                                </li>
-                                <li>
-                                <NavLink exact to="/login" >Login</NavLink>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+            <nav class="navbar navbar-expand-lg" >
+                <h1 class="navbar-brand"><a href="index.html">byPlantas</a></h1>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="active">
+                            <NavLink exact to="/" >Inicio</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/catalogo" >Catálogo</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/categorias" >Categorias</NavLink>
+                        </li>
+                        <li>
+                            <NavLink exact to="/login" >Login</NavLink>
+                        </li>
+                    </ul>
                 </div>
-            </div>
+            </nav>
         </header>
     )
 }
