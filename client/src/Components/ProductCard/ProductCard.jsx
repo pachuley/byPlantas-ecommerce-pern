72131b4bc6cf47c9e87eb81a5e028a1b18efbd8a
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-24 21:29:19 -0300

    (S12): created spinner component

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -2,25 +2,24 @@ import React from 'react';
 import style from './productCard.module.css'
 import { Link } from 'react-router-dom';
 
-const ProductCard = (props) =>{
-
-const { nameProduct, descriptionProduct, priceProduct, urlProduct, stockProduct, id} = props;
-    return(
-  
-        <div className={`${style.boxes}`}>
-            <Link to={`/products/${id}`}>
-            <img className={`${style.img}`} src={urlProduct}/>
-            <ul>{nameProduct}</ul>
-            
-            <ul>ARS {priceProduct}</ul>
-            <ul>{stockProduct}</ul>
-            </Link>
-        </div>
-        
-
-
-    )
+const ProductCard = ({ 
+    nameProduct, 
+    descriptionProduct, 
+    priceProduct, 
+    urlProduct, 
+    stockProduct, 
+    id
+}) =>{
+        return(
+            <div className={`${style.boxes}`}>
+                <Link to={`/products/${id}`}>
+                <img className={`${style.img}`} src={urlProduct}/>
+                <ul>{nameProduct}</ul>
+                
+                <ul>ARS {priceProduct}</ul>
+                <ul>{stockProduct}</ul>
+                </Link>
+            </div>
+        )
 }
-
-
 export default ProductCard;
