1760642384b21ed2eea42009d1816095ba680307
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-25 09:17:32 -0300

    add cors to server

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
@@ -9,7 +10,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
