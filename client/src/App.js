c0070c45bc39c8813dfa9417b6c53d6e8fcc4ddf
unknown <lucas.a.fernandez.morales@gmail.com>
2021-01-24 21:02:03 -0300

    Feature/ Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Switch, Route} from 'react-router-dom'
+import NavBar from './Components/Navbar/Navbar';
+import Footer from './Ui_Components/Footer/Footer';
+import Catalog from './Components/Catalog/Catalog';
 import Product from './Components/Product/Product'
-import Catalog from './Components/Catalog/Catalog'
-
-import ProductContainer from './Components/Product/Product';
-
+import FormProduct from './Components/FormProduct/FormProduct'
+import FormCategory from './Components/FormCategory/FormCategory'
 
 function App() {
   return (
     <>
+      {/* <NavBar /> */}
       <Switch>
         <Route path="/products" exact render={() => <Catalog/>}/>
         <Route path='/products/:id' render={({match}) => <Product match={match}/>}/>
-        <Route path='/products' exact render={() => <FormProduct/>}/>
-        <Route path='/products/category' render={() => <FormCategory/>}/>
+        <Route path='/addProduct' exact render={() => <FormProduct/>}/>
+        <Route path='/addCategory' exact render={() => <FormCategory/>}/>
       </Switch>
+      <Footer />
     </>
   );
 }
