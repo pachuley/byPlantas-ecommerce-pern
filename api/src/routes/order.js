30a09ef5b6203d21203d6eeb300649e8756c1ea6
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-26 23:43:42 -0300

    feature/order_routes creted method pust order

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,4 +1,22 @@
 const server = require("express").Router();
+const { Order, User } = require("../db.js");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+server.put('/:id', (req,res,next) => {
+    let {date, status, id} = req.body
+    Order.update(
+        {
+            date,
+            status
+        },
+        { returning: true, where: { id } }
+    )
+    .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch(next);
+})
 
 
 
