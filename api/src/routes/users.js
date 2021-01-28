0a59d35acd8cce6b8387bb37fc3a88816ebe83b7
Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2021-01-28 14:26:58 -0300

    Se agrega tarea S45

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -52,4 +52,12 @@ server.put('/:id', async (req, res) => {
     }
 });
 
+// S45 : Crear Ruta que retorne todas las Ordenes de los usuarios
+// GET /users/:id/orders
+server.get('/:id/orders', (req, res, next) => {
+    Order.findAll({where:{ id: req.params.id }})
+        .then(orders => res.status(201).json(orders))
+        .catch(error => res.send(201).json({message:"We couldn't find your request"}))
+})  
+
 module.exports = server;
