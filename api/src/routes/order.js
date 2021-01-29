11bcaa1d194d712fbbc684e8dd1317d9c08e94bf
laura <laura.aristegui@hotmail.com>
2021-01-29 13:01:15 -0300

    fix usersID/cart

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -20,58 +20,6 @@ server.put('/:id', (req,res,next) => {
 
 
 
-server.post('/:userId/cart', (req, res) => {
-    console.log(req.body);
-    const currentItem ={ 
-        productId: req.body.productId,
-        userId: req.params.idUser,
-        quantity: req.body.quantity,
-        price: req.body.price,
-        total: (req.body.quantity * req.body.price)
-    }
-    Order.findOne({ 
-           where: { 
-               userId: req.params.idUser, 
-               status: "active"
-            } 
-        })
-        .then((order) => {
-            order.addProduct({ 
-                productId: req.body.productId,
-                userId: req.params.idUser,
-                quantity: req.body.quantity,
-                price: req.body.price,
-                total: (req.body.quantity * req.body.price)
-             })
-             .then(Orderline => res.status(201).json({message: "Item was added to Order"}))
-             .catch(error => {
-                 res.status(400).json({message: "Internal error"})
-             })
-        })
-    })
-
-     
-//vaciar carrito
-server.put('/:userId/cart', (req, res) => {
-    Order.findOne({ where: { userId: req.params.userId, status: "active" } })
-        .then((orders) => {
-            Orderline.update({
-                price:"",
-                quantity:"",
-                discount:"",
-                total:""
-            }, {
-             where:{id: orderId } 
-            })
-        
-           .then(
-            res.status(200).json({ message: "El carrito fue vaciado" })
-                )
-        })
-        .catch(function (err) {
-            res.status(400).json({ message: "No se pudo vaciar el carrito.", error: err })
-        })
-})
 
 
 
