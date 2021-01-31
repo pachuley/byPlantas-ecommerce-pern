c55a0e3d78bbf952518e205402aa8ce548684ca7
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-01-31 20:08:06 -0300

    connect catalog container to redux

--- client/src/Components/CatalogContainer/CatalogContainer.jsx
+++ client/src/Components/CatalogContainer/CatalogContainer.jsx
@@ -4,93 +4,113 @@ import axios from 'axios'
 import Catalog from '../Catalog/Catalog'
 import FiltersContainer from '../FiltersContainer/FiltersContainer'
 import Spinner from '../Spinner/Spinner'
+import {connect} from 'react-redux'
+import {fetchProducts} from '../../Redux/actions/productsActions'
 
 const {REACT_APP_BACKEND_URL} = process.env;
 
 /* 
     Componente contenedor de los filtros y el listado del catalogo
 */
-const CatalogContainer = () => {
-    const [products, setProducts] = useState([])
+const CatalogContainer = (props) => {
     const [categories, setCategories] = useState([])
     const [categorySelected, setCategorySelected] = useState('')
     const [inpfilter, setInpfilter] = useState('')
-    const [loading, setLoading] = useState(false)
+    const [sort, setSort] = useState('')
 
     useEffect(()=>{
-        getProducts()
+        props.dispatch(fetchProducts())
         getCategories()
     }, [])
+
+    const sortProducts = event => {
+        setSort(event.target.value) 
+    }
     
     const getCategories = () => {
-        setLoading(true)
         axios.get(`${REACT_APP_BACKEND_URL}/category`)
             .then(res => {
                 setCategories(res.data)
-                setLoading(false)
-            })
-    }
-    const getProducts = async () => {
-        setLoading(true)
-        axios.get(`${REACT_APP_BACKEND_URL}/products`)
-            .then(res => {
-                setProducts(res.data)
-                setLoading(false)
             })
     }
 
-    const handleCategory = (category) => {
-        setCategorySelected(category.name)
+    const handleCategory = (event) => {
+        setCategorySelected(event.target.value)
     }
     const handleInputFilter = (event) => {
         setInpfilter(event.target.value)
     }
-    /* (product.name.concat(product.description)).toLowerCase().includes(inpfilter.toLocaleLowerCase()) */
     const productsFiltered = () => {
         if(!categorySelected && !inpfilter){
-            return products.filter(product => 
+            return props.products.filter(product => 
                 (product.stock > 0))
         }
         if(!categorySelected && inpfilter){
-            return products.filter(product => 
+            return props.products.filter(product => 
                 (product.stock > 0) && (product.name.concat(product.description)).toLowerCase().includes(inpfilter.toLocaleLowerCase()))
         }
-        return products.filter(product => {
+        return props.products.filter(product => {
             return product.categories.some(cat => cat.name === categorySelected) && (product.name.concat(product.description)).toLowerCase().includes(inpfilter.toLocaleLowerCase())
         })
     }
+ 
+    let sortProductos = () => {
+        let aux = productsFiltered()
+        if(sort === 'menorPrecio'){
+            return aux.sort((a,b) => parseFloat(a.price) - parseFloat(b.price))
+        }
+        if(sort === 'mayorPrecio'){
+            return aux.sort((a,b) => parseFloat(b.price) - parseFloat(a.price))
+        }
+        return aux
+    } 
     const handleCleanFilters = () => {
         setCategorySelected('')
         setInpfilter('')
+        setSort('')
     }
-    
     return ( 
         <Fragment>
-            {loading ? 
+            {props.isFetching ? 
                 <Spinner />
                     :
-                    <div className="container my-5">
-                        <div className="row pt-5">
-                            <div className={`col-3 p-2 ${style.border}`}>
-                                <FiltersContainer 
-                                    categories={categories}
-                                    handleCategory={handleCategory}
-                                    categorySelected={categorySelected}
-                                    handleInputFilter={handleInputFilter}
-                                    handleCleanFilters={handleCleanFilters}
-                                    value={inpfilter}
-                                />
+                    <div className="container-fluid px-5">
+                        <div className="row">
+                            <div className="col-9">
+                                <div className={`col-12 p-2 ${style.border}`}>
+                                    <FiltersContainer 
+                                        categories={categories}
+                                        handleCategory={handleCategory}
+                                        categorySelected={categorySelected}
+                                        handleInputFilter={handleInputFilter}
+                                        handleCleanFilters={handleCleanFilters}
+                                        value={inpfilter}
+                                        count={productsFiltered().length}
+                                        sortProducts={sortProducts}
+                                    />
+                                </div>
+                                <div className={`col-12 p-2`}>
+                                    <Catalog 
+                                        products={sortProductos()}
+                                    />
+                                </div>
                             </div>
-                            <div className={`col-9 p-2`}>
-                                <Catalog 
-                                    products={productsFiltered()}
-                                />
+                            <div className="col-3">
+                                carrito
                             </div>
+                            
                         </div>
                     </div>
             }
         </Fragment>
      );
 }
- 
-export default CatalogContainer;
+
+const mapStateToProps = (state) => {
+    return {
+        products: state.products.products,
+        isFetching: state.products.isFetching
+    }
+    
+}
+export default connect(mapStateToProps)(CatalogContainer)
