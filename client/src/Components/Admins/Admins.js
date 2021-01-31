e620123dc078ef71f62669e4f2151ff534641f54
Lucas <laguero90@gmail.com>
2021-01-31 14:44:13 -0300

    fix admins logic and ordes route

--- client/src/Components/Admins/Admins.js
+++ client/src/Components/Admins/Admins.js
@@ -14,7 +14,7 @@ const Admins = (props) => {
       
         <div className="d-flex flex-row bd-highlight mb-3">
           <div className="p-2 bd-highlight">
-            <Link to='/admin' className="btn btn-sm btn-success mr-3">ADMIN - Listar y modificar ORDENES</Link>
+            <Link to='/orders' className="btn btn-sm btn-success mr-3">ADMIN - Listar y modificar ORDENES</Link>
           </div>
           <div className="p-2 bd-highlight">
           <Link to='/productslist' className="btn btn-sm btn-success">ADMIN - Inventario de Productos y Categorias</Link>
