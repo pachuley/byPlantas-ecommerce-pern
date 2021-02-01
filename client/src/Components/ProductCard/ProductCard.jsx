44e35c753bd717dfef34bfbdba559a81fdb62401
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-01 00:12:06 -0300

    fix product card

--- client/src/Components/ProductCard/ProductCard.jsx
+++ client/src/Components/ProductCard/ProductCard.jsx
@@ -10,20 +10,20 @@ const ProductCard = ({
     imgs, 
     stock, 
     id
-}) => (
+}) =>{
 
-            <div className="card mt-3">
-              <img  src={imgs ? imgs : "https://cdn.iconscout.com/icon/premium/png-256-thumb/coming-soon-label-842108.png"} className="img-thumbnail" />
-              <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text"> Stock: {stock}</p>
-                <p className="card-text"> Precio: {price}</p>
-                <NavLink to={`/products/${id}`}>
-                  <button className="btn btn-primary">Product Details</button >
-                </NavLink>
-            </div>
-          </div>
-            
-        )
-
+  return(
+    <div className="card">
+      <NavLink to={`/products/${id}`}>
+        <img  src={imgs ? imgs : "https://cdn.iconscout.com/icon/premium/png-256-thumb/coming-soon-label-842108.png"} className="img-thumbnail" />
+      </NavLink>
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className='card-text'>Precio: ARS$ {price}</p>
+        <p className="card-text">Stock: {stock}</p>
+        <BtnCart className="btn btnByPlantas" productId={id} stock={stock} name={name} price={price} imgs={imgs} />
+      </div>
+    </div>
+  )
+}
 export default ProductCard;
