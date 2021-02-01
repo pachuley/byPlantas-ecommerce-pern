39f6d4be00fb3082f483481d0d9fc3f2cf57fae3
HomeroHerrero4r6 <homero.herrero.92@gmail.com>
2021-02-01 04:24:40 -0300

    cambios en Componente OrderTable

--- client/src/Components/OrderTable/OrderTable.jsx
+++ client/src/Components/OrderTable/OrderTable.jsx
@@ -1,56 +1,60 @@
-import React, {useEffect,useState}from 'react';
-import {Link} from 'react-router-dom';
-import './orderTable.module.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./orderTable.module.css";
+import axios from "axios";
 // import axios from 'axios';
-const {REACT_APP_BACKEND_URL} = process.env;
-
-
+const { REACT_APP_BACKEND_URL } = process.env;
 
 export default function OrderTable() {
-    const [orders, setOrders] = useState([])
-    
-    useEffect(()=>{
-        console.log('ejecuta');
-        getOrders()
-    },[])
-    const getOrders = async () => {
-        console.log('orders')
-        axios.get(`${REACT_APP_BACKEND_URL}/orders`)
-            .then(res => {
-                setOrders(...orders, res.data)
-            })
-    }
-    console.log(orders)
-    return (
-        <div className="container tabla">
-            <div className="col-md-10 panel-right row tabla" style={{ paddingTop: '25px' }}>
-            <div className="col-md-12 col-lg-12">
-                <h2 className="text-center">Todas las Ordenes</h2>
-                <table className="table table-hover table-dark thfontsize">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id User</th>
-                            <th scope="col">Id Order</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">F. Compra</th>
-                            <th scope="col">Detalles</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {orders.map(order=> 
-                        <tr key={order.id}>
-                            <td>{order.userId}</td>
-                            <td>{order.id}</td>
-                            <td>{order.status}</td>
-                            <td>{order.createdAt}</td>
-                            <td><button className={`btn btn-link`}><Link to={`admin/orders/${order.id}`}>detalle</Link></button></td>
-                        </tr>
-                       )}
-                    </tbody>
-                </table>
-            </div>
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    console.log("ejecuta");
+    getOrders();
+  }, []);
+  const getOrders = async () => {
+    console.log("orders");
+    axios.get(`${REACT_APP_BACKEND_URL}/orders`).then((res) => {
+      setOrders(...orders, res.data);
+    });
+  };
+  console.log(orders);
+  return (
+    <div className="container tabla">
+      <div
+        className="col-md-10 panel-right row tabla"
+        style={{ paddingTop: "25px" }}
+      >
+        <div className="col-md-12 col-lg-12">
+          <h2 className="text-center">Todas las Ordenes</h2>
+          <table className="table table-hover table-dark thfontsize">
+            <thead>
+              <tr>
+                <th scope="col">Id User</th>
+                <th scope="col">Id Order</th>
+                <th scope="col">Status</th>
+                <th scope="col">F. Compra</th>
+                <th scope="col">Detalles</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order.id}>
+                  <td>{order.userId}</td>
+                  <td>{order.id}</td>
+                  <td>{order.status}</td>
+                  <td>{order.createdAt}</td>
+                  <td>
+                    <button className={`btn btn-link`}>
+                      <Link to={`admin/orders/${order.id}`}>detalle</Link>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
