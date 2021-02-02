3eb766aa691c9c77baeb894c08a515be79b31428
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-02 11:56:22 -0300

    I modified the login form with formik

--- client/src/Components/FormLogin/FormLogin.jsx
+++ client/src/Components/FormLogin/FormLogin.jsx
@@ -3,37 +3,35 @@ import axios from 'axios';
 import styles from './formlogin.module.css'
 import { connect } from 'react-redux';
 import Swal from 'sweetalert2'
+import {useFormik} from 'formik'
 const {REACT_APP_BACKEND_URL} = process.env;
 
-export default function FormLogin (){
-    const [user, setUser] = useState({email: '', password: ''})
+const validate = values => {
+    const errors = {};
 
-    const handleChange = e => {
-        setUser({
-            ...user,
-            [e.target.name]:e.target.value
-        })
+    if (!values.email) {
+      errors.email = 'Email es requerido';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Email inválido';
     }
+    if (!values.password) {
+        errors.password = 'Password es requerida';
+      }
+  
+    return errors;
+  };
 
-    const emailPattern = new RegExp(/[A-Za-z0-9_.]+\@\w+\.\w\w+/, 'i'); //valida que tenga un '@' seguido de un string seguido de '.' seguido por lo menos 2 caracteres
-    const passwordPattern = new RegExp(/[A-Za-z0-9_.]{8,}/, 'i'); //valida que tengo por lo menos 8 caracteres
-    const handleSubmit = e => {
-        e.preventDefault()
-        console.log()
-        if(!emailPattern.test(user.email)){
-            Swal.fire({
-                title: 'Email inválido',
-                icon: 'error'
-            })
-        }else if(!passwordPattern.test(user.password)){
-            Swal.fire({
-                title: 'Contraseña inválida',
-                icon: 'error'
-            })
-        }else{
-            axios.post(`${REACT_APP_BACKEND_URL}/users/login`, user)
-            .then(resp=>{console.log(resp)
-                user.email === "admin@admin.com" ? localStorage.setItem('admin', 'true') : localStorage.setItem('admin', 'false')
+export default function FormLogin (){
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: '',
+        },
+        validate,
+        onSubmit: values => {
+            axios.post(`${REACT_APP_BACKEND_URL}/users/login`, values)
+            .then(resp=>{
+                values.email === "admin@admin.com" ? localStorage.setItem('admin', 'true') : localStorage.setItem('admin', 'false')
                 changeLogin(resp.data.userId)
                 Swal.fire({
                     title: `${resp.data.message}`,
@@ -43,9 +41,11 @@ export default function FormLogin (){
                 window.location.reload();
             })
             .catch(err=>{console.log(err)})
-        }
-        
+
     }
+      })
+
+
     const changeLogin = (userId) => {
         const dataLogin = {userId: userId}
         localStorage.setItem('Login', JSON.stringify(dataLogin))
@@ -53,7 +53,7 @@ export default function FormLogin (){
 
     return (
         <div className='container col-md-6 justify-content-center'>
-            <form className={` w-50 py-3 needs-validation mx-auto`} onSubmit={handleSubmit}>
+            <form className={` w-50 py-3 needs-validation mx-auto`} onSubmit={formik.handleSubmit}>
                 <h4 className={`${styles.titles}`}>Ingresa a tu cuenta!</h4>
                 <label htmlFor='inputLoginEmail' className='form-label'>Escribe tu Email</label>
                 <input 
@@ -62,9 +62,12 @@ export default function FormLogin (){
                     className='form-control' 
                     type='email' 
                     placeholder='Email...' 
-                    value={user.email} 
-                    onChange={handleChange} 
-                    required/>
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                    />
+                     {formik.errors.email ? <p className="my-2 error">{formik.errors.email}</p> : null}
+
                 <label htmlFor='inputLoginPassword' className='form-label'>Escribe tu Contraseña</label>
                 <input 
                     id='inputLoginPassword' 
@@ -72,10 +75,18 @@ export default function FormLogin (){
                     className='form-control' 
                     type='password' 
                     placeholder='Password' 
-                    value={user.password} 
-                    onChange={handleChange} 
-                    required/>
-                <button className={`btn mt-2 mb-3 my-auto btnByPlantas`} type='submit'>Ingresa</button>
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    />
+                    {formik.errors.password ? <p className="my-2 error">{formik.errors.password}</p> : null}
+                <button 
+                    className={`btn mt-2 mb-3 my-auto btnByPlantas`} 
+                    type='submit'
+                    disabled={Object.keys(formik.errors).length > 0}
+                    >
+                        Ingresa
+                </button>
             </form>
         </div>
     )
