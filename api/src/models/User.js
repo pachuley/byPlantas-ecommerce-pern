98f9901df8a63b7a2fb4dd7637a39cd59909eb7e
HomeroHerrero4r6 <homero.herrero.92@gmail.com>
2021-02-03 16:03:20 -0300

    Agregando autenticacion con jwt, creamos middlewares, etc

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,15 +1,26 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 
-
 module.exports = (sequelize) => {
   // defino el modelo
-  const User = sequelize.define('user', {
+  const User = sequelize.define("user", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    birthdate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique:true,
+      unique: true,
       validate: {
         isEmail: true,
       },
@@ -17,6 +28,15 @@ module.exports = (sequelize) => {
     encryptedPassword: {
       type: DataTypes.TEXT,
       allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      allowNull: true,
+      values: ["CLIENT_ROLE", "ADMIN_ROLE", "VENDOR_ROLE"],
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   });
 };
