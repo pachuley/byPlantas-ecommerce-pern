0b278fb06dce9d5b3173f34df375cabf7f6f25f6
pachuley <fjsabate@gmail.com>
2021-02-04 21:41:48 -0300

    Funciona el Carruseeeeeeel!

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,9 +1,7 @@
 import React, {Fragment} from 'react';
 import styles from './home.module.css'
 import {Link} from 'react-router-dom'
-import foto1 from './Images/Carrusel/foto1.png'
-import foto2 from './Images/Carrusel/foto2.png'
-import foto3 from './Images/Carrusel/foto3.png'
+import ControlledCarousel from '../Carousel/Carousel'
 
 const Home = () => {
     return ( 
@@ -24,49 +22,19 @@ const Home = () => {
                         </div>
                     </div>
                 </section>
+
                 <section>
                     <div className={`d-flex flex-column ${styles.filosofia}`}>
                         <h1 className={`${styles.titles}`}>Nuestra filosofia</h1>
                         <p className={`${styles.filosofiaP}`}>Vender productos producidos de manera sustentable bajo la ideología de cuidar el medioambiente sin dejar de lado la calidad que el público espera de las mejores marcas!</p>
                     </div>
                 </section>
-                <section>
 
-                    <div id="carruselOfertas" class="carousel slide" data-bs-ride="carousel">
-                        <h1 className={`${styles.titles}`}>Ofertas</h1>
-                        <div className="carousel-inner">
-                            <div className={`carousel-item active`}>  
-                                <img src={foto1} class="d-block w-85"/>
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>First slide label</h5>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </div>
-                            </div>
-                            <div className={`carousel-item`}>
-                                <img src={foto2} class="d-block w-85"/>
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>First slide label</h5>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </div>
-                            </div>
-                            <div className={`carousel-item`}>
-                                <img src={foto3} class="d-block w-85"/>
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h5>First slide label</h5>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <a className="carousel-control-prev" data-target="carruselOfertas" role="button" data-bs-slide="prev">
-                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Anterior</span>
-                        </a>
-                        <a className="carousel-control-next" data-target="carruselOfertas" role="button" data-bs-slide="next">
-                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span className="visually-hidden">Posterior</span>
-                        </a>
-                    </div>
+                <section>
+                    <h1 className={`${styles.titles}`}>Ofertas de la Semana</h1>
+                    <ControlledCarousel/>
                 </section>
+                
                 <section>
                     <div className={`${styles.contacts}`}>
                         <form>
