076f33bd8f63294ef8380109efaff654e3e11b98
pachuley <fjsabate@gmail.com>
2021-02-06 19:42:28 -0300

    Diseños de Tarjetas de reviews

--- client/src/Components/ReviewContainer/ReviewCard.jsx
+++ client/src/Components/ReviewContainer/ReviewCard.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import styles from './reviewcard.module.css'
+import StarRatingComponent from 'react-star-rating-component';
+import { FaLeaf } from 'react-icons/fa';
 
 const ReviewCard = ({title,comment,stars,name,lastname}) => {
-    return ( 
-        <div className={`${styles.cardreview}`}>
-            <h5 className={`${styles.cardtitle}`}>"{title}"</h5>
-            <p>{comment}</p>
-            <div className={`${styles.namecard} d-flex justify-content-end`}>
-                    <cite>-{name}</cite>
-                    {/* <cite className="px-1">{lastname}</cite> */}
-            </div>
-            <div className="d-flex justify-content-end my-2">
-                {stars}
-            </div>
+    return (
 
+        <div className={`${styles.cardreview}`}>
+            <div className={`${styles.titlecontainer}`}>
+                <h5 className={`${styles.cardtitle}`}>"{title}"</h5>
+                <StarRatingComponent 
+                    name="rate2" 
+                    editing={false}
+                    renderStarIcon={() => <span><FaLeaf size={17}/></span>}
+                    starCount={5}
+                    value={stars}
+                />
+            </div>
+            <div className={`${styles.cardbody}`}>
+                <p>{comment}</p>
+                <div className={`${styles.namecard} d-flex justify-content-end`}>
+                        <cite>-{name}</cite>
+                        {/* <cite className="px-1">{lastname}</cite> */}
+                </div>
+            </div>
         </div>
      );
 }
