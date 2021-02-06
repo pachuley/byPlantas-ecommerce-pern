c6b3c80196a7b73e3b54144d7cc1315f5aa61d43
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-06 17:05:46 -0300

    add the login logic in navbar

--- client/src/Ui_Components/Navbar/Navbar.jsx
+++ client/src/Ui_Components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './navbar.module.css'
 import Logout from '../Logout/Logout'
+import { useDispatch, useSelector} from 'react-redux'
+import {logout} from '../../Redux/actions/userActions'
 
-export default function NavBar() {
+
+const NavBar = () => {
     //invoco al Localstorage para levantar si el admin es true o no.
     let admin = localStorage.getItem('admin')
-    console.log(admin)
     //
+    const userLogin = useSelector(state => state.userLogin)
+    const dispatch = useDispatch()
+    
     return (
         <header id="header" className={`fixed-top ${styles.header, styles.headerText}`}>
             <nav className={`navbar navbar-expand-lg ${styles.byPlantasNavbar}`} >
-                <h1 className={`navbar-brand mr-auto ${styles.h1Margin, styles.headerLogo}`}><a className={styles.linkLogo} href="/">byPlantas</a></h1>
+                <h1 className={`navbar-brand mr-auto ${styles.h1Margin, styles.headerLogo}`}><Link className={styles.linkLogo} to="/">byPlantas</Link></h1>
                 <button className={`navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className={`navbar-toggler-icon`}></span>
                 </button>
@@ -23,21 +28,21 @@ export default function NavBar() {
                             
                         </li>
                         <li className={`${styles.liMargin}`}>
-                            <NavLink className={styles.link} activeClassName={styles.alink} exact to="/login" >LogIn</NavLink>
-                        </li>
-                        <li className={`${styles.liMargin}`}>
                             {admin !== "true" ?
                             <NavLink className={styles.link} activeClassName={styles.alink} exact to="/cart" >Carrito</NavLink>
                             : ""}
                         </li>
                         <li className={`${styles.liMargin}`}>
-                        { admin === "true" ?
-                            <NavLink className={styles.link} activeClassName={styles.alink} exact to="/admins" >Admin</NavLink>
-                            : ""}
+                            {userLogin.userLogin ? (
+                                <Logout />
+                            ) : 
+                            
+                                <NavLink className={styles.link} activeClassName={styles.alink} exact to="/login" >LogIn</NavLink>
+                            }
                         </li>
                         <li className={`${styles.liMargin}`}>
                         { admin === "true" ?
-                            <Logout />
+                            <NavLink className={styles.link} activeClassName={styles.alink} exact to="/admins" >Admin</NavLink>
                             : ""}
                         </li>
                     </ul>
@@ -46,3 +51,5 @@ export default function NavBar() {
         </header>
     )
 }
+
+export default NavBar
