dcf1de258de94f1815a4cb450c42d6f6d6584c91
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-11 14:44:15 -0300

    feature/cart add action update order to cart

--- client/src/Components/Commons/BtnCheckout/BtnCheckout.jsx
+++ client/src/Components/Commons/BtnCheckout/BtnCheckout.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import styles from './btncheckout.module.css'
-const BtnCheckout = ({orderId,disabled}) => {
+const BtnCheckout = ({orderId,disabled,handleCheckout}) => {
     let disabledLink = disabled ? styles.disabledlink : ''
+
     return ( 
         <div>
             <Link 
                 className="btn btn-sm btn-secondary"
                 to={`/checkout/${orderId}`} 
                 className={`${disabledLink} btn btn-sm btn-secondary`}   
+                onClick={handleCheckout}
             >
                 Checkout
             </Link>
