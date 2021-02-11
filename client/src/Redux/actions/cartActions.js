4ab00577cdd2104106e908106c336949eed1ecaa
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-10 21:21:05 -0300

    feature/cart create getOrderlines action

--- client/src/Redux/actions/cartActions.js
+++ client/src/Redux/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, FETCH_ITEMS_REQUEST, FETCH_ITEM_ERROR } from '../types';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, FETCH_ITEMS_REQUEST, FETCH_ITEM_ERROR, CART_ITEMS_GET } from '../types';
 const { REACT_APP_BACKEND_URL } = process.env;
 
 //TODO: ver stock en action
@@ -13,8 +13,8 @@ export const addToCart = (id, quantity) => async (dispatch, getState) => {
     },
   };
   let discount = 0;
+
   try {
-    const res = await axios.get(`${REACT_APP_BACKEND_URL}/products/${id}`, config);
 
     const { data } = await axios.post(
       `${REACT_APP_BACKEND_URL}/users/${isAuth.id}/cart`,
@@ -68,3 +68,28 @@ export const removeFromCart = id => async (dispatch, getState) => {
     });
   }
 };
+
+export const getItems = () => async (dispatch,getState) => {
+  dispatch({type: FETCH_ITEMS_REQUEST})
+  const isAuth = getState().userLogin.userLogin;
+  let config = {
+    headers: {
+      'Content-Type': 'application/json',
+      token: isAuth ? isAuth.token : null,
+    },
+  };
+ 
+  try {
+    const {data} = await axios.get(`${REACT_APP_BACKEND_URL}/users/${isAuth.id}/cart`,config)
+    dispatch({
+      type: CART_ITEMS_GET,
+      payload: data,
+    });
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+  } catch (error) {
+    dispatch({
+      type: FETCH_ITEM_ERROR,
+      error: error.toString(),
+    });
+  }     
+}
