ddefb6a2404b21e1f49aa1d9c0193f1092f232d4
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-13 17:40:29 -0300

    feature/cart action remove all items

--- client/src/Redux/actions/cartActions.js
+++ client/src/Redux/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, FETCH_ITEMS_REQUEST, FETCH_ITEM_ERROR, CART_ITEMS_GET } from '../types';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, FETCH_ITEMS_REQUEST, FETCH_ITEM_ERROR, CART_ITEMS_GET, } from '../types';
 const { REACT_APP_BACKEND_URL } = process.env;
 
 //TODO: ver stock en action
@@ -68,6 +68,33 @@ export const removeFromCart = id => async (dispatch, getState) => {
     });
   }
 };
+
+export const removeAllItems = () => async (dispatch,getState) => {
+  const isAuth = getState().userLogin.userLogin;
+  let config = {
+    headers: {
+      'Content-Type': 'application/json',
+      token: isAuth ? isAuth.token : null,
+    },
+  };
+  getState().cart.cartItems.forEach(async element => {
+    try {
+      const { data } = await axios.delete(`${REACT_APP_BACKEND_URL}/users/${isAuth.id}/cart/${element.productId}`, config);
+      dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: data.id,
+      });
+  
+      localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+    } catch (error) {
+      dispatch({
+        type: FETCH_ITEM_ERROR,
+        error: error.toString(),
+      });
+    }
+  });
+}
+
 
 export const getItems = () => async (dispatch,getState) => {
   dispatch({type: FETCH_ITEMS_REQUEST})
