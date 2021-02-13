5ea7be6729ef5dd3f9db6e0538c5902702506248
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-13 17:46:59 -0300

    feature/cart remove all items action y checkout

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -3,17 +3,19 @@ import CartLine from '../CartLine/CartLine'
 import BtnCheckout from '../Commons/BtnCheckout/BtnCheckout'
 import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {getItems} from '../../Redux/actions/cartActions'
+import {getItems, removeAllItems} from '../../Redux/actions/cartActions'
 import {updateOrder} from '../../Redux/actions/orderActions'
+import Swal from 'sweetalert2'
+import { useHistory } from "react-router"
 
 const Cart = () => {
-
-  let userLocalstorage = JSON.parse(localStorage.getItem('userInfo'))
 
   const userLogin = useSelector(state => state.userLogin)
   const cartItems = useSelector(state => state.cart.cartItems)
   const dispatch=useDispatch()
   const isAuth =  userLogin.userLogin
+  let history = useHistory()
+
   const totalCart = () => {
       let totalCart = 0
       cartItems.forEach(element => {
@@ -24,18 +26,65 @@ const Cart = () => {
     useEffect(()=>{
       if(isAuth){
         dispatch(getItems())
+        dispatch(updateOrder(orderid,'active'))
       }
     },[])
     const orderid = cartItems[0]?.orderId
-    const handleCheckout = () => {
-      dispatch(updateOrder(orderid,'processing'))
-        console.log(orderid)
+
+
+    const handleCheckout = async() => {
+      const result = await Swal.fire({
+        title: 'Esta seguro de comenzar con el checkout?',
+        icon: 'info',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, comenzar!',
+        cancelButtonText: 'Cancelar'
+      })
+      if(result.isConfirmed){
+          dispatch(updateOrder(orderid,'processing'))
+          history.push(`/checkout/${orderid}`)
+      }
+    }
+
+
+    const handleRemoveAll = () => {
+      Swal.fire({
+        title: 'Esta seguro de eliminar los items?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, eliminar!',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          dispatch(removeAllItems())
+          Swal.fire(
+            'Eliminados!',
+            'Su carrito fue vaciado',
+            'success'
+          )
+        }
+      })
     }
     
     return (
       <div className="container">
-        <div className="row">
-          <h5 className={`m-0 py-1`}>Aquí están los productos que elegiste</h5>
+        <div className="row py-2">
+          <div className="col-6">
+            {/* <h5 className={`m-0 py-1`}>Aquí están los productos que elegiste</h5> */}
+          </div>
+          <div className="col-6">
+            <button 
+              className="btn btn-sm btn-danger" 
+              onClick={handleRemoveAll}
+              disabled={cartItems.length === 0 ? true : false}
+              >
+                Vaciar
+              </button>
+          </div>
         </div>
         <div className="row">
           <div className="col-8">
