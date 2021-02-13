b1d4e62be4b77e0caa4b04ad73511d053cce1f91
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-13 17:47:59 -0300

    feature/cart changes to button checkout component

--- client/src/Components/Commons/BtnCheckout/BtnCheckout.jsx
+++ client/src/Components/Commons/BtnCheckout/BtnCheckout.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import styles from './btncheckout.module.css'
-const BtnCheckout = ({orderId,disabled,handleCheckout}) => {
+const BtnCheckout = ({disabled,handleCheckout}) => {
     let disabledLink = disabled ? styles.disabledlink : ''
 
     return ( 
-        <div>
-            <Link 
+        <div> 
+            <button 
                 className="btn btn-sm btn-secondary"
-                to={`/checkout/${orderId}`} 
                 className={`${disabledLink} btn btn-sm btn-secondary`}   
                 onClick={handleCheckout}
             >
                 Checkout
-            </Link>
+            </button>
         </div>
      );
 }
