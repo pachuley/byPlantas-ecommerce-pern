30da1d7f2dc4e6b9a4769bfa24df993dc5d32087
pachuley <fjsabate@gmail.com>
2021-02-13 11:15:31 -0300

    Estilos en el checkout

--- client/src/Components/CheckOut/CheckOut.jsx
+++ client/src/Components/CheckOut/CheckOut.jsx
@@ -2,13 +2,13 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux';
 import {Redirect} from 'react-router-dom'
+import styles from './checkout.module.css'
 import CartCheckOut from '../CartCheckOut/CartCheckOut'
 import { useSelector } from 'react-redux'
 import Payment from '../Payment/Payment'
 import ShippingDetail from '../ShippingDetail/ShippingDetail';
+
 const {REACT_APP_BACKEND_URL} = process.env;
-
-
 const CheckOut = ()=>{
 
   //Levanto los datos del local para poder enviar con la variante config todos los datos del token
@@ -29,34 +29,28 @@ const CheckOut = ()=>{
     const [cart, setCart] = useState([])
     const [total, setTotal] = useState(0)
 
-    
-    
-    
-console.log(cartItems)
-    return (
-        <div>
-            
-              <h2 className={`m-0 text-left p-5`}>Aquí están los productos que elegiste</h2>
-
-              <div className={'m-0 text-left p-2 l-5'}>
-                  {cart ? cartItems.map((product,index)=>(
-                      <div key={index}>
-                          <CartCheckOut product={product} />
-                      </div>
-                  )) : <h1>El carrito esta vacio</h1>}                
-              </div>
-             
-              <div className='text-center mt-5 container'>
-                  <Payment/>
-              </div>
-              <div className='text-center mt-5 container'>
-                  <ShippingDetail/>
-              </div>
-            
+  return (
+    <div className={`${styles.checkoutContainer}`}>
+      <div>
+        <h2 className={`m-0 text-left p-5`}>¿Estas listo para comprar?</h2>
+      </div>
+      <div className={`${styles.checkout}`}>
+        <div className={`col-8`}>
+            {cart ? cartItems.map((product,index)=>(
+                <div key={index}>
+                    <CartCheckOut product={product} />
+                </div>
+            )) : <h1>El carrito esta vacio</h1>}                
         </div>
-    )
+        <div className='text-center container col-3'>
+            <Payment/>
+        </div>
+      </div> 
+        <div className='text-center mt-2 container'>
+            <ShippingDetail/>
+        </div>
+    </div>
+  )
 }
-
-
 
 export default CheckOut;
