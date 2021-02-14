031de8090b06697ddc70d830b1e91bf3304a8608
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-14 11:05:59 -0300

    feature/cart added option to show cart guest and cart in cart component

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -3,19 +3,22 @@ import CartLine from '../CartLine/CartLine'
 import BtnCheckout from '../Commons/BtnCheckout/BtnCheckout'
 import {useSelector, useDispatch} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {getItems, removeAllItems} from '../../Redux/actions/cartActions'
+import {getItems, removeAllItems,removeAllItemsGuest,joinCarts} from '../../Redux/actions/cartActions'
 import {updateOrder} from '../../Redux/actions/orderActions'
 import Swal from 'sweetalert2'
 import { useHistory } from "react-router"
 import styles from './cart.module.css'
+import {FaTrashAlt,FaPlusSquare} from 'react-icons/fa'
 
 const Cart = () => {
 
   const userLogin = useSelector(state => state.userLogin)
   const cartItems = useSelector(state => state.cart.cartItems)
+  const cartItemsGuest = useSelector(state => state.cart.cartItemsGuest)
   const dispatch=useDispatch()
   const isAuth =  userLogin.userLogin
   let history = useHistory()
+  console.log(cartItemsGuest.length > 0 )
 
   const totalCart = () => {
       let totalCart = 0
@@ -52,7 +55,7 @@ const Cart = () => {
 
     const handleRemoveAll = () => {
       Swal.fire({
-        title: 'Esta seguro de eliminar los items?',
+        title: 'Esta seguro de vaciar el carrito?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -61,16 +64,26 @@ const Cart = () => {
         cancelButtonText: 'Cancelar'
       }).then((result) => {
         if (result.isConfirmed) {
-          dispatch(removeAllItems())
-          Swal.fire(
+          if(cartItems.length > 0){
+            dispatch(removeAllItems())
+            Swal.fire(
+              'Eliminados!',
+              'Su carrito fue vaciado',
+              'success'
+            )
+          }
+          if(cartItemsGuest.length > 0){
+            dispatch(removeAllItemsGuest())
+            Swal.fire(
             'Eliminados!',
-            'Su carrito fue vaciado',
+            'Su carrito fue vaciado (Guest)',
             'success'
-          )
+            )
+          } 
         }
       })
     }
-    
+
     return (
       <div className="container">
         <div className="row py-2">
@@ -81,22 +94,46 @@ const Cart = () => {
             <button 
               className="btn btn-sm btn-danger" 
               onClick={handleRemoveAll}
-              disabled={cartItems.length === 0 ? true : false}
+              disabled={cartItems.length === 0 && cartItemsGuest.length === 0 ? true : false}
               >
-                Vaciar
+                Vaciar <FaTrashAlt className="ml-1"/>
               </button>
           </div>
         </div>
         <div className={`${styles.cart}`}>
           <div className={`${styles.cartlinesContainer} col-9`}>
           {
-            cartItems.length === 0 ? 
-            <div className="alert alert-primary" role="alert">
-              El carrito está vacío
-            </div>
+            isAuth !== null && (cartItems.length === 0 && cartItemsGuest.length === 0) ? 
+              <div className="alert alert-primary" role="alert">
+                El carrito está vacío
+              </div>
             : cartItems.map((product, index) => {
               return <CartLine product={product} />
             })
+          }
+          {
+            (cartItemsGuest.length > 0) ? 
+            <>
+                <div className="alert alert-primary d-flex justify-content-between" role="alert">
+                  <span>Productos que agrego como invitado</span>
+                  <div>
+                  <button 
+                    className="btn btn-success btn_sm mx-1"
+                    onClick={()=>{dispatch(joinCarts())}}
+                  ><FaPlusSquare/></button>
+                  <button 
+                    className="btn btn-success btn_sm"
+                    onClick={() => {dispatch(removeAllItemsGuest())}}
+                    ><FaTrashAlt/></button>
+                  </div>
+                </div>
+                {
+                  cartItemsGuest.map((product, index) => {
+                  return <CartLine product={product} />
+                })
+                }
+            </>
+            : null
           }
           </div>
           <div className="col-2">
