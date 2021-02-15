405eceff9f4eaf38deb8f41ff67a5b85d0273478
pachuley <fjsabate@gmail.com>
2021-02-15 20:18:21 -0300

    Cambios al carrito

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -93,20 +93,9 @@ const Cart = () => {
     }
 
     return (
-      <div className="container">
+      <div className={`${styles.cartContainer}`}>
         <div className="row py-2">
-          <div className="col-6">
-            <h5 className={`m-0 py-1`}>Aquí están los productos que elegiste</h5>
-          </div>
-          <div className="col-6">
-            <button 
-              className="btn btn-sm btn-danger" 
-              onClick={handleRemoveAll}
-              disabled={cartItems.length === 0 && cartItemsGuest.length === 0 ? true : false}
-              >
-                Vaciar <FaTrashAlt className="ml-1"/>
-              </button>
-          </div>
+            <h2 className={`m-0 text-center p-5`}>Aquí están los productos que elegiste</h2>
         </div>
         <div className={`${styles.cart}`}>
           <div className={`${styles.cartlinesContainer} col-9`}>
@@ -126,7 +115,7 @@ const Cart = () => {
                   <span>Productos que agrego como invitado</span>
                   <div>
                   <button 
-                    className="btn btn-success btn_sm mx-1"
+                    className="btn btnByPlantas btn_sm mx-1"
                     onClick={joinCartHandle}
                     disabled={isAuth === null ? true : false}
                   ><FaPlusSquare/></button>
@@ -145,21 +134,33 @@ const Cart = () => {
             : null
           }
           </div>
-          <div className="col-2">
-                <div className={`text-center`}>Precio ({cartItems.length}) items</div>
-                <p className="text-center mt-1">Total: ${totalCart()}</p>
-                {
-                  isAuth ? 
-                  <BtnCheckout
-                    disabled={orderid === undefined ? true : false}
-                    orderId={orderid !== undefined ? cartItems[0].orderId : ''}
-                    handleCheckout={handleCheckout}
-                  />
-                  :
-                  <Link to='/login' className="btn btnByPlantas btn-sm">
-                    Ingresa a tu cuenta!
-                  </Link>
-                }
+          <div className={`${styles.checkoutContainer} col-2`}>
+                <span className={`${styles.price} text-center`}>Precio ({cartItems.length}) items</span>
+                <span className={`${styles.total} text-center`}>Total: ${totalCart()}</span>
+                <div className={`${styles.checkoutButtonContainer}`}>
+                  {
+                    isAuth ? 
+                    <BtnCheckout
+                      disabled={orderid === undefined ? true : false}
+                      orderId={orderid !== undefined ? cartItems[0].orderId : ''}
+                      handleCheckout={handleCheckout}
+                    />
+                    :
+                    <Link to='/login' className="btn btnByPlantas btn-sm">
+                      Ingresa a tu cuenta!
+                    </Link>
+                  }
+                </div>
+                <div className={`${styles.trashButtonContainer}`}>
+                  <span className={`${styles.total} text-center`}>Vaciar carrito</span>
+                  <button 
+                    className={`btn ${styles.trashButton}`} 
+                    onClick={handleRemoveAll}
+                    disabled={cartItems.length === 0 && cartItemsGuest.length === 0 ? true : false}
+                    >
+                      Vaciar <FaTrashAlt className="ml-1"/>
+                  </button>
+              </div>
             </div>
           </div>
         </div>
